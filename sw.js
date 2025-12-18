85f539ba85d1cc36ca52d87d26a29758120cd631
prashantwct <prasarsenal@gmail.com>
2025-12-18 09:48:44 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'vhf-tri-v1';
+const ASSETS = [
+  '/',
+  '/static/app.js',
+  '/manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
